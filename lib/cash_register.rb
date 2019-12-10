7874fafc1a2c13a8e363643c18e48b12ba4e1b54
Calvin Baker <calvin.baker128@gmail.com>
2019-12-10 19:57:06 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,27 @@
+require 'pry'
+class CashRegister
+  def initialize(discount=nil)
+    @total = 0
+    if discount
+      @discount = discount
+    end
+  end
+  
+  attr_accessor :total, :discount
+  
+  def add_item(title, price, quantity=nil)
+    @prev_total = @total
+    @total += price
+  end
+  
+  
+  def void
+    @total = @prev_total
+    "You have removed #{items.pop} from the cart."
+  
+  def apply_discount
+  end
+end
 
+
+[1, 2, 3, 4, 5].pop #=> 5
